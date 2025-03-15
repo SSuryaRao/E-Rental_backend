64a77f98c0fb70bc8a63d856bb5f35731c9dd949
SSuryaRao <suryaraosodi@gmail.com>
2025-03-15 19:11:41 +0530

    adding comment

--- src/db/db.js
+++ src/db/db.js
@@ -6,11 +6,12 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, );
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
-        
+        // error finding
     } catch (error) {
-        console.log("MongoDB connection failed ",error);
+        console.log("MongoDB connection failed ",error); 
         process.exit(1);
     }
 }
+
 
 export default connectDB
