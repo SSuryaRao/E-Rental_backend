e123738cf2b46b2b6b3c10d48f5848036fc894c6
SSuryaRao <suryaraosodi@gmail.com>
2025-03-20 19:42:18 +0530

    adding error handing like async handler , apierror etc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import dotenv from 'dotenv';
 import connectDB from './db/db.js';
-
-
-
-connectDB();
+import express from 'express';
 
 dotenv.config({
     path: './env'
+})
+
+const app = express();
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+    app.on("error", (error) => {
+        console.log("error in the server ",error);
+        throw error;
+    })
+})
+.catch((err) => {
+    console.log("Error in mongo db connection", err);   
+})
+
+app.get('/', (req,res) => {
+    res.send(`server is running on port ${process.env.PORT || 8000}`);
 })
