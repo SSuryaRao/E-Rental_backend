1874465755e21ddb1e51638908266a1a478f5467
SSuryaRao <suryaraosodi@gmail.com>
2025-04-17 19:15:43 +0530

    add cart routes to the application

--- src/app.js
+++ src/app.js
@@ -18,9 +18,12 @@ app.use(cors({
 import { userRouter } from './routes/user.routes.js';
 import { sellerRouter } from './routes/seller.routes.js';
 import { productRouter } from './routes/product.routes.js';
+import { cartRouter } from './routes/cart.routes.js';
 
 app.use('/api/v1/user',userRouter);
 app.use('/api/v1/seller',sellerRouter);
 app.use('/api/v1/product',productRouter);
+app.use('/api/v1/cart',cartRouter);
+
 
 export { app }
